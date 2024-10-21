b6f60e169adbe3794991a94b0b6760c39f44ab77
reduct0r <yanshinti@student.bmstu.ru>
2024-10-21 15:32:37 +0300

    fix breeds logic

--- app/src/main/java/com/example/homework_project_1/MainActivity.kt
+++ app/src/main/java/com/example/homework_project_1/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.homework_project_1.ui.theme.Homework_project_1Theme
 
 class MainActivity : ComponentActivity() {
