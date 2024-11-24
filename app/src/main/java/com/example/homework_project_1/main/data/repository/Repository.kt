bb4bfee2eaf029819bb0a6b69ebd54fedafe415e
reduct0r <yanshinti@student.bmstu.ru>
2024-11-25 01:45:51 +0300

    delete unused

--- app/src/main/java/com/example/homework_project_1/main/data/repository/Repository.kt
+++ app/src/main/java/com/example/homework_project_1/main/data/repository/Repository.kt
@@ -1,7 +1,6 @@
 package com.example.homework_project_1.main.data.repository
 
 import com.example.homework_project_1.main.data.model.JokeDTO
-import com.example.homework_project_1.main.data.model.JokeEntity
 
 interface Repository {
     suspend fun fetchJokes(amount: Int): List<JokeDTO>
